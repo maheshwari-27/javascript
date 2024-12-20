386266c9ec439d43b1dcd1defd513e4af1a95b41
kaderu-27 <kaderuuma@gmail.com>
2024-12-20 17:38:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*arthematic operators*/
+let a=10,b=20;
+console.log(`the additon of ${a} and ${b} is ${a+b}`);
+console.log(`the subtraction of ${a} and ${b} is ${a-b}`);
+console.log(`the multiplication  of ${a} and ${b} is ${a*b}`);
+console.log(`the division of ${a} and ${b} is ${a/b}`);
+console.log(`the modules of ${a} and ${b} is ${a%b}`);
+/* relational operators*/
+let a1=50,b2='20';
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+console,log(a==b);
+console.log(a===b);
+console.log(a!==b);
+console.log(a!=b);
