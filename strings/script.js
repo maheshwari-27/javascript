6f426f9ba80fcbfff202fde5ed1410c68b5ede52
kaderu-27 <kaderuuma@gmail.com>
2024-12-20 18:12:27 +0530

    first commit

---
+++ strings/script.js
@@ -0,0 +1,11 @@
+let s='fight is common';
+console.log(s.toUpperCase())
+console.log(s.startsWith("fi"))
+console.log(s.endsWith("s"))
+console.log(s.replace('f','l'))
+let s1=' kite in the air ';
+console.log(s1.split("") )
+console.log(s1.split( "  "))
+console.log(s1.slice(2))
+console.log(s1.slice(3,5))
+console.log(s1.trim())
